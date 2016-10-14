49737f5fbaa792074c6561f929297047a41f671f
Sebastien Robert <sebastien.robert@insidesales.com>
2016-10-14 17:14:46 -0400

    Add acceptance test to validate hooks copy
     See also [Issue #2](https://github.com/johnmartel/git-hook-gradle-plugin/issues/2)

--- src/test/groovy/acceptanceTest/ThePluginCanBeAppliedToAProject.groovy
+++ src/test/groovy/acceptanceTest/ThePluginCanBeAppliedToAProject.groovy
@@ -1,5 +1,6 @@
 package acceptanceTest
 
+import org.apache.commons.io.filefilter.NameFileFilter
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
@@ -8,6 +9,8 @@ import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
 class ThePluginCanBeAppliedToAProject extends Specification {
+
+    private static final String CONFIG_GIT_HOOKS_FOLDER_NAME = 'config/githooks'
 
     @Rule
     TemporaryFolder projectDir = new TemporaryFolder()
@@ -37,5 +40,40 @@ class ThePluginCanBeAppliedToAProject extends Specification {
 
         then:
         buildResult.task(':copyGitHooks').outcome == TaskOutcome.SUCCESS
+    }
+
+
+    def """given the git-hook-gradle-plugin is applied to a project
+        when I run the copyGitHooks task
+        the provided hooks are copied to ./git/hooks folder"""() {
+
+        given:
+
+        File targetGitHooksFolder = projectDir.newFolder('.git', 'hooks')
+        File sourceGitHooksFolder = projectDir.newFolder('config', 'githooks')
+        projectDir.newFile("${CONFIG_GIT_HOOKS_FOLDER_NAME}/commit-msg")
+
+        File buildFile = projectDir.newFile('build.gradle')
+        buildFile << """
+            plugins {
+                id 'ca.coglinc.githook'
+            }
+
+            repositories {
+                jcenter()
+            }
+        """
+
+        when:
+        BuildResult buildResult = GradleRunner.create()
+                .withProjectDir(projectDir.getRoot())
+                .withArguments('copyGitHooks')
+                .withPluginClasspath()
+                .build();
+
+        then:
+        FilenameFilter filter = new NameFileFilter('commit-msg')
+        String[] files = targetGitHooksFolder.list(filter)
+        files?.length == 1
     }
 }
